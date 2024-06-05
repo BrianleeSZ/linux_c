a036eeda04235a91548f53a03c7974492e4f76f6
Admin <lishuai82man@gmail.com>
2024-06-05 15:45:27 +0800

    完善timer定时器，增加googletest

--- time/timer.cpp
+++ time/timer.cpp
@@ -6,8 +6,12 @@
 #include <sys/epoll.h>
 #include <memory>
 #include <unistd.h>
+#include <gtest/gtest.h>
 using namespace std;
-
+/**
+ * @brief 面试现场手撕定时器
+ * 简单处理
+*/
 struct TimerBaseNode{
     time_t expire;
     int64_t id;
@@ -49,10 +53,24 @@ public:
         // 将std::chrono::milliseconds转换为time_t类型
         return static_cast<time_t>(ms.count());
     }
-    TimerBaseNode AddTimer(time_t msec,TimerNode::Callback func){
+     TimerBaseNode AddTimerOld(time_t msec,TimerNode::Callback func){
         time_t expire = GetTick() + msec;
+        //cout << "AddTimer:" << GetTick() << endl;
+        
         auto pairs = timeouts.emplace(GenID(),expire,std::move(func));
         return static_cast<TimerBaseNode>(*pairs.first);
+    }
+    TimerBaseNode AddTimer(time_t msec,TimerNode::Callback func){
+        time_t expire = GetTick() + msec;
+        //cout << "AddTimer:" << GetTick() << endl;
+        if(timeouts.empty() || expire < timeouts.crbegin()->expire){
+            auto pairs = timeouts.emplace(GenID(),expire,std::move(func));
+            return static_cast<TimerBaseNode>(*pairs.first);
+        }
+        //O(1) 插入的节点 大于 最大值
+        //cout <<"优化点......"<<endl;
+        auto ele = timeouts.emplace_hint(timeouts.crbegin().base(),GenID(),expire,std::move(func));
+        return static_cast<TimerBaseNode>(*ele);
     }
     bool DelTimer(TimerBaseNode &node){
        auto iter = timeouts.find(node);    //c++14 等价key的特性
@@ -96,25 +114,64 @@ private:
     set<TimerNode,std::less<>> timeouts;
 };
 
-int main(){
-    int epfd = epoll_create(1);;
+int64_t Timer::gid = 0;
+// int main(){
+//     int epfd = epoll_create(1);;
+//     unique_ptr<Timer> timer = make_unique<Timer>();
+
+//     epoll_event ev[256]={0};
+
+//     int i = 0;
+//     timer->AddTimer(1000, [&](const TimerNode &node)
+//     {   
+//         i++;
+//         cout << "node id:" << node.id << " hello brian i= " << i << "expire = " << node.expire << endl;
+//     });
+//     timer->AddTimer(1000, [&](const TimerNode &node)
+//     {   
+//         i++;
+//         cout << "node id:" << node.id << " hello brian i= " << i << "expire = " << node.expire << endl;
+//     });
+//     while (true)
+//     {
+//         int n = epoll_wait(epfd,ev,256, timer->TimeToSleep());
+//         time_t now = Timer::GetTick();
+//         for (int i = 0; i < n; i++)
+//         {
+//             //处理网络事件
+//         }
+//         //处理定时事件
+//         timer->HandleTimer(now);
+//     }
+    
+//     close(epfd);
+    
+//     return 0;
+// }
+
+TEST(Timer,AddTimerOld){
     unique_ptr<Timer> timer = make_unique<Timer>();
-
-    epoll_event ev[256]={0};
-
-    while (true)
+    int t = 0;
+    for (int i = 0; i < 10000; i++)
     {
-        int n = epoll_wait(epfd,ev,256, timer->TimeToSleep());
-        time_t now = Timer::GetTick();
-        for (int i = 0; i < n; i++)
-        {
-            //处理网络事件
-        }
-        //处理定时事件
-        timer->HandleTimer(now);
+       timer->AddTimerOld(3000, [&](const TimerNode &node)
+       {   
+            t++;
+            //cout << "node id:" << node.id << " hello brian i= " << i << "expire = " << node.expire << endl;
+       });
     }
     
-    close(epfd);
-    
-    return 0;
+}
+
+TEST(Timer, AddTimer){
+    unique_ptr<Timer> timer = make_unique<Timer>();
+    int t = 0;
+    for (int i = 0; i < 10000; i++)
+    {
+       timer->AddTimer(3000, [&](const TimerNode &node)
+       {   
+            t++;
+            //cout << "node id:" << node.id << " hello brian i= " << i << "expire = " << node.expire << endl;
+       });
+    }
 }
